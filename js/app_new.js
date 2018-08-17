83981df6b6ea18880b4bccca88cfebe3a3f38236
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2018-08-17 14:41:07 -0400

    Add new app again

---
+++ js/app_new.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app_new',
+
+  data: {
+    hello: 'Hello World, again!',
+    names: [
+      {firstname: 'John', lastname: 'Doe'},
+      {firstname: 'Jane', lastname: 'Jones'},
+      {firstname: 'Will', lastname: 'Smith'},
+    ]
+  }
+})
