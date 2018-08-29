c80ded95085225e4b44c7afa52dc6b4099f17d20
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2018-08-29 16:02:19 -0400

    put movielist and single movie into templates, set up routes

--- js/app_new.js
+++ js/app_new.js
@@ -1,47 +1,21 @@
 apiURL = 'http://d8vue.dd:8083/api/movies'
 
-var hello = Vue.extend({
-  template: '#hello',
-  data: function(){
-    return {
-      message: 'Hello World'
-    }
-  }
-})
-
-var aboutus = Vue.extend({
-  template: '#about-us'
-})
 
 var App = Vue.extend({});
-var router = new VueRouter();
 
-router.map({
-  '/hello' : {
-    component: hello
+var movieList = Vue.extend({
+  template: '#movie-list-template',
+  data: function() {
+    return {
+      movies: '',
+      liveFilter: '',
+      genreFilter: '',
+      genres: ''    
+    }
   },
-  '/about-us': {
-    component: aboutus
-  }
-});
-
-router.start(App, '#app_new');
-
-new Vue({
-  el: '#app_new',
-
-  data: {
-    movies: '',
-    liveFilter: '',
-    genreFilter: '',
-    genres: '',
-    movie: ''
-  },
-
   ready: function(){
     this.getMovies();
   },
-
   methods: {
     getMovies: function(){
       this.$set('movie', '');
@@ -56,18 +30,86 @@ new Vue({
             
           }) 
         })
-        //console.log(JSON.stringify(genresArr));
         this.$set('genres', genresArr);
-        //console.log(movies);
       });
-    },
-    getTheMovie: function(movieID){
-      this.$http.get(apiURL + '/' + movieID, function(movie){
-        this.$set('movie', movie);
-        //console.log(JSON.stringify(movie));
-      })
-      //alert(movieID);
     }
+  }
+});
 
+var singleMovie = Vue.extend({
+  template: '#single-movie-template',
+  data : function() {
+    return {
+      movie: ''
+    }
+  },
+  ready: function(){
+    this.getTheMovie();
+  },
+  methods: {
+    getTheMovie: function(){
+      this.$http.get(apiURL + '/' + this.$route.params.movieID, function(movie){
+        this.$set('movie', movie);
+      })
+    } 
   }
 })
+
+var router = new VueRouter();
+
+router.map({
+  '/' : {
+    component: movieList
+  },
+  'movie/:movieID' : {
+    name: 'movie',
+    component: singleMovie
+  }
+});
+
+router.start(App, '#app_new');
+
+// new Vue({
+//   el: '#app_new',
+
+//   data: {
+//     movies: '',
+//     liveFilter: '',
+//     genreFilter: '',
+//     genres: '',
+//     movie: ''
+//   },
+
+//   ready: function(){
+//     this.getMovies();
+//   },
+
+//   methods: {
+//     getMovies: function(){
+//       this.$set('movie', '');
+//       this.$http.get(apiURL, function(movies){
+//         this.$set('movies', movies);
+//         genresArr = [];
+//         jQuery.each(movies, function(index, movie){
+//           jQuery.each(movie.field_genres, function(index, genre){
+//             if (jQuery.inArray(genre.value, genresArr) === -1) {
+//               genresArr.push(genre.value);
+//             }
+            
+//           }) 
+//         })
+//         //console.log(JSON.stringify(genresArr));
+//         this.$set('genres', genresArr);
+//         //console.log(movies);
+//       });
+//     },
+//     getTheMovie: function(movieID){
+//       this.$http.get(apiURL + '/' + movieID, function(movie){
+//         this.$set('movie', movie);
+//         //console.log(JSON.stringify(movie));
+//       })
+//       //alert(movieID);
+//     }
+
+//   }
+// })
